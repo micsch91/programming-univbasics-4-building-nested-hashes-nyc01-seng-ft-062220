ff23b9e07dd3c74ffc526cbdbdd3655a4cb7f203
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 19:50:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,10 @@ def monopoly_with_second_tier
   {:boy => "Make sure you have at least one key defined in the monopoly hash"}
   {:railroads =>
     {:pieces => 4,
-      :rent_in_dollars => {},
-      
+      :rent_in_dollars => {
+        :one_piece_owned => 25
+      },
+
       :names => {}
 
   }
