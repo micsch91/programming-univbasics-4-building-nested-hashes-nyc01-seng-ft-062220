1145d1d802f1f878aaadba2e753827e15c4bf572
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 19:41:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-{:railroads => "Make sure you have at least one key defined in the monopoly hash"}
+{:boy => "Make sure you have at least one key defined in the monopoly hash"}
+:railroads => {
+
+}
 end
 
 def monopoly_with_second_tier
