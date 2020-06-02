8b12d06049a42042ed14fc3f60b0b3f21e44c04d
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 19:49:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
   # complexity.
 
   {:boy => "Make sure you have at least one key defined in the monopoly hash"}
-  {:railroads => 
+  {:railroads =>
     {:pieces => 4,
       :rent_in_dollars => {},
       
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
   # complexity.
 
   {:boy => "Make sure you have at least one key defined in the monopoly hash"}
-  {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}, 
+  {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {},
 
   }
     }
